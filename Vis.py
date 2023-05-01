375545608bc646578296f2b01fd845d30402a9cd
Ralhazmy1 <raghid.alhazmy@gmail.com>
2023-05-01 15:58:13 -0500

    started on coordinates to zone conversion

--- Vis.py
+++ Vis.py
@@ -1,11 +1,10 @@
 import osmnx as ox
-import osmnx.utils_geo
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
 
-def map_events_to_zones(nyc_taxi_geo: gpd.geodataframe, crashes: pd.DataFrame, closures: pd.DataFrame):
+def map_events_to_zones(nyc_taxi_geo: gpd.geodataframe, crashes: pd.DataFrame, closures: pd.DataFrame)
     """
     Maps crashes and closures to
     :param nyc_taxi_geo:
@@ -13,25 +12,11 @@ def map_events_to_zones(nyc_taxi_geo: gpd.geodataframe, crashes: pd.DataFrame, c
     :param closures:
     :return:
     """
-    pass
-
-def zone_center(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    gdf['centroid'] = gdf['geometry'].centroid
-    # print(gdf)
-    return gdf
 
 
-def distance_between_two_zones(gdf:gpd.GeoDataFrame, zone1: int, zone2: int):
-    zone1_bbox = gdf.loc['object_id' == zone1]['geometry']
-    g_z1 = osmnx.graph_from_polygon(zone1_bbox)
-    random_address_z1 = osmnx.utils_geo.sample_points(g_z1,1)
-    zone2_bbox = gdf.loc['object_id' == zone2]['geometry']
-    g_z2 = osmnx.graph_from_polygon(zone2_bbox)
-    random_address_z2 = osmnx.utils_geo.sample_points(g_z2, 1)
-    # zone1_centroid = gdf.loc['object_id' == zone1]['centroid']
-    # zone2_centroid = gdf.loc['object_id' == zone2]['centroid']
-    distance = random_address_z1.distance(random_address_z2)
-    return distance
+def zone_center():
+
+def distance_between_two_zones():
 
 
 def events_during_trips(trips, crashes, closures):
@@ -47,14 +32,14 @@ def events_during_trips(trips, crashes, closures):
         pass
 
 
-# zone_slide = ipywidgets.IntSlider(value=2, min=2, max=263)
-# @ipywidgets.interact(zone = zone_slide)
-# def plot_zone_speeds(df:pd.DataFrame, zone:int):
-#     sub1 = df[df['PULocationID'] == zone]
-#     sub2 = df[df['DOLocationID'] == zone]
-#     sub = pd.concat([sub1,sub2])
-#     # print((sub))
-#     plt.hist(sub['avg speed'], bins = 60)
-#     plt.xlabel('Average Speed (mph)')
-#     plt.ylabel('Number of trips')
+zone_slide = ipywidgets.IntSlider(value=2, min=2, max=263)
+@ipywidgets.interact(zone = zone_slide)
+def plot_zone_speeds(df:pd.DataFrame, zone:int):
+    sub1 = df[df['PULocationID'] == zone]
+    sub2 = df[df['DOLocationID'] == zone]
+    sub = pd.concat([sub1,sub2])
+    # print((sub))
+    plt.hist(sub['avg speed'], bins = 60)
+    plt.xlabel('Average Speed (mph)')
+    plt.ylabel('Number of trips')
     return plt.show()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import geopandas as gpd
 import numpy as np
+from shapely.geometry import Point
 
 
 def open_file(path: str) -> pd.DataFrame:
@@ -13,7 +14,7 @@ def open_file(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
 
-def format_index(df:pd.DataFrame,new_index_name)->pd.DataFrame:
+def format_index(df: pd.DataFrame, new_index_name: str) -> pd.DataFrame:
     df.rename(columns={'Unnamed: 0': new_index_name}, inplace=True)
     df.set_index(new_index_name)
     return df
@@ -47,13 +48,18 @@ def find_neighbors(gdf: gpd.GeoDataFrame) -> dict:
         # remove own zone number from the list
         neighbors = [int(num) for num in neighbors if zone.objectid != num]
 
-        # add zone neighbors to neighbor dictionary
+        #add zone neighbors to neighbor dictionary
         neighbor_dict[index + 1] = neighbors
 
     return neighbor_dict
 
-
 def filter_trips_based_on_zones(df:pd.DataFrame, neighbor_dict: dict):
+    """
+
+    :param df:
+    :param neighbor_dict:
+    :return:
+    """
     trips_zones_dict = {}
     # keep only trips that have PO and DO zones that aren't above 263
     limit_mask = (df['PULocationID'] < 264) & (df['DOLocationID'] < 264)
@@ -68,31 +74,32 @@ def filter_trips_based_on_zones(df:pd.DataFrame, neighbor_dict: dict):
     no_neighbors_mask = (~df['PULocationID'].isin(dont_include)) & (~df['DOLocationID'].isin(dont_include))
 
     # apply masks
-
-    masks = pd.concat((limit_mask, no_neighbors_mask), axis=1)
-    select = masks.all(axis=1)
-    trips_df = df[select]
+    trips_df = df[limit_mask]
+    trips_df = trips_df[no_neighbors_mask]
 
     for index, trip in trips_df.iterrows():
 
-        pu_zone = trip['PULocationID']
+        PUzone = trip['PULocationID']
 
-        pu_zone_neighbor_neighbors = []
-        pu_zone_neighbors = neighbor_dict[pu_zone]
-        pu_zone_neighbor_neighbors.append(pu_zone_neighbors)
+        PUzone_neighbor_neighbors = []
+        PUzone_neighbors = neighbor_dict[PUzone]
+        PUzone_neighbor_neighbors.append(PUzone_neighbors)
 
-        for i in pu_zone_neighbors:
-            pu_zone_neighbor_neighbors.append(neighbor_dict[i])
+        for i in PUzone_neighbors:
+            PUzone_neighbor_neighbors.append(neighbor_dict[i])
 
-        pu_zone_neighbor_neighbors = [int(x) for x in list(np.concatenate(pu_zone_neighbor_neighbors).flat)]
+        PUzone_neighbor_neighbors = [int(x) for x in list(np.concatenate(PUzone_neighbor_neighbors).flat)]
 
-        trips_zones_dict[trip['tripID']] = set(pu_zone_neighbor_neighbors)
+        trips_zones_dict[trip['tripID']] = set(PUzone_neighbor_neighbors)
     exclude = []
     for x in trips_df.iterrows():
-        trip_id = x[1][0]
-        do_zone = x[1][9]
-        if do_zone not in (trips_zones_dict[trip_id]):
-            exclude.append(trip_id)
+        tripID = x[1][0]
+        PUzone = x[1][8]
+        DOzone = x[1][9]
+        # print(PUzone,DOzone)
+        if DOzone not in (trips_zones_dict[tripID]):
+            # print(tripID)
+            exclude.append(tripID)
     trips_df = trips_df[~trips_df['tripID'].isin(exclude)]
     return trips_df
 def datetime_conversions(df: pd.DataFrame, column_names: list, time_format: str) -> pd.DataFrame:
@@ -106,7 +113,14 @@ def datetime_conversions(df: pd.DataFrame, column_names: list, time_format: str)
 
 
 def add_time_and_speed(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculates the trip time and average speed of a taxi trip and creates two columns in the taxi dataframe
+     to store the data
+    :param df: taxi pandas dataframe
+    :return: The taxi dataframe with trip_time_h and avg speed columns
+    """
     df['trip_time'] = (df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime'])
+
     df['trip_time_h'] = [(x.total_seconds()) / 3600 for x in df['trip_time']]
     df['avg speed'] = df['trip_distance'] / df['trip_time_h']
     return df
@@ -114,18 +128,18 @@ def add_time_and_speed(df: pd.DataFrame) -> pd.DataFrame:
 
 def removeWeirdTaxiData(df: pd.DataFrame) -> pd.DataFrame:
     """
-    :param df: taxi trip df
-    :return: taxi trip df without trips that are less than a day or longer than a minute and without trips
-    where the average trip speed is less than 90mph or greater than 1mph
+
+    :return:
     """
     too_quick = df['trip_time_h'] >= 0.01666666667
     too_long = df['trip_time_h'] <= 24
     super_fast = df['avg speed'] <= 90
     super_slow = df['avg speed'] >= 1
+    df = df[too_quick]
+    df = df[too_long]
+    df = df[super_fast]
+    df = df[super_slow]
 
-    masks = pd.concat((too_quick, too_long, super_fast, super_slow), axis=1)
-    select = masks.all(axis=1)
-    df = df[select]
     return df
 
 def change_location_to_zones(df, locations_column_names):
@@ -142,9 +156,9 @@ if __name__ == '__main__':
     taxi_data = format_index(open_file("sampled_combined_taxi_2018_600k.csv"),'tripID')
 
     # remove taxi columns
-    taxi_data = keep_relevant_columns(taxi_data, ['tripID','tpep_pickup_datetime', 'tpep_dropoff_datetime',
-                                                  'trip_distance', 'PULocationID', 'DOLocationID',
-                                                  'fare_amount', 'tip_amount', 'tolls_amount', 'total_amount'])
+    taxi_data = keep_relevant_columns(taxi_data, ['tripID','tpep_pickup_datetime', 'tpep_dropoff_datetime', 'trip_distance',
+                                                  'PULocationID', 'DOLocationID', 'fare_amount', 'tip_amount',
+                                                  'tolls_amount', 'total_amount'])
     # datetime_conversions
     taxi_data = datetime_conversions(taxi_data, ['tpep_pickup_datetime', 'tpep_dropoff_datetime'],
                                      '%m/%d/%Y %I:%M:%S %p')
@@ -154,21 +168,29 @@ if __name__ == '__main__':
     # neighbors and zones
     nyc_taxi_geo = gpd.read_file('NYC_Taxi_Zones.geojson')
     neighbors = find_neighbors(nyc_taxi_geo)
-    taxi_data = filter_trips_based_on_zones(taxi_data, neighbors)
+    # taxi_data = filter_trips_based_on_zones(taxi_data, neighbors)
 
 
     # open crashes file
     crashes_data = format_index(open_file("2018_crashes.csv"), 'index')
-    print(crashes_data.columns)
+
     # remove columns
     crashes_data = keep_relevant_columns(crashes_data, ['index', 'CRASH DATE_CRASH TIME', 'LOCATION'])
-    # crashes_data['Date-time_of_crash'] = crashes_data[['DATE_CRASH', 'TIME']].agg('-'.join, axis=1)
-    # crashes_data = datetime_conversions(crashes_data, ['Date-time_of_crash'], '%Y-%m-%d-%H:%M:%S')
+    crashes_data = datetime_conversions(crashes_data, ['CRASH DATE_CRASH TIME'], '%Y-%m-%d %H:%M:%S')
 
+    # get zone from coordinates
+    crashes_data['ZONE'] = ''
+    taxi_zones = nyc_taxi_geo.zone
+    taxi_zones_boundaries = nyc_taxi_geo.geometry.boundary
+    for index, row in crashes_data.iterrows():
+        for index, boundary in taxi_zones_boundaries:
+            print(type(taxi_zones))
+            if row['Location'].within(boundary):
+                row['ZONE'] = boundary.zone  # record zone  in ['ZONE'] column
+    ###### TRY TO VECTORIZE LATER
 
     #open file
     street_closures = open_file("2018_street_closures.csv")
-
     #remove columns
     street_closures = keep_relevant_columns(street_closures, ['FROMSTREETNAME', 'TOSTREETNAME',
                                                               'WORK_START_DATE', 'WORK_END_DATE'])
